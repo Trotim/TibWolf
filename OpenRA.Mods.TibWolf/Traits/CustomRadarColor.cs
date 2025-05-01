35f7cdaaad85c8e60dfea859bc29a803403c8e18
Trotim <Trotim@users.noreply.github.com>
2025-05-01 12:31:22 +0100

    Add CustomRadarColor from CA and test

---
+++ OpenRA.Mods.TibWolf/Traits/CustomRadarColor.cs
@@ -0,0 +1,30 @@
+﻿using OpenRA.Mods.Common.Traits;
+using OpenRA.Primitives;
+using OpenRA.Traits;
+
+namespace OpenRA.Mods.TibWolf.Traits
+{
+	[Desc("Define a custom radar color used regardless of owner.")]
+	public class CustomRadarColorInfo : TraitInfo
+	{
+		[Desc("Color to use")]
+		public readonly Color Color = Color.White;
+
+		public override object Create(ActorInitializer init) { return new CustomRadarColor(init, this); }
+	}
+
+	public class CustomRadarColor : IRadarColorModifier
+	{
+		public CustomRadarColorInfo Info { get; set; }
+
+		public CustomRadarColor(ActorInitializer init, CustomRadarColorInfo info)
+		{
+			Info = info;
+		}
+
+		Color IRadarColorModifier.RadarColorOverride(Actor self, Color color)
+		{
+			return Info.Color;
+		}
+	}
+}
